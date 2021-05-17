c301af2681d18ecacfd8bc8e9318659a483e62fc
ms-manish <84309808+ms-manish@users.noreply.github.com>
2021-05-17 12:52:27 +0530

    Add files via upload

---
+++ GitJenkinsDemo.java
@@ -0,0 +1,33 @@
+
+import java.io.*;
+public class GitJenkinsDemo
+{
+    public static void main(String[] args)
+    {
+        int rows=7;            
+        for (int i= 0; i<= rows-1 ; i++)
+        {
+            for (int j=0; j <i; j++)
+            {
+                System.out.print(" ");
+            }
+            for (int k=i; k<=rows-1; k++) 
+            {
+            System.out.print("*" + " ");
+            } 
+            System.out.println("");
+            } 
+            for (int i= rows-1; i>= 0; i--)
+        {
+            for (int j=0; j< i ;j++)
+            {
+                System.out.print(" ");
+            }
+            for (int k=i; k<=rows-1; k++)
+            {
+                System.out.print("*" + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
