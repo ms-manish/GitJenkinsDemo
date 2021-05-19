d7f948ceea253346c6cd9f945912128ecf475f0f
ms-manish <84309808+ms-manish@users.noreply.github.com>
2021-05-19 14:57:11 +0530

    Create firstpipeline.groovy

---
+++ firstpipeline.groovy
@@ -0,0 +1,20 @@
+pipeline{
+agent any
+stages {
+  stage('Build') {
+    step {
+      sh 'echo "This is my first step"'
+    }
+  }
+  stage('Test') {
+    step
+      sh 'echo "This is my Test step"'
+    }
+  }
+  stage('Deploy') {
+    steps {
+      sh 'echo "This is my Deploy step"'
+    }
+}
+  }
+}
